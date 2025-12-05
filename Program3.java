f6ed6f9e532ba68fd75c63bb1fd9cd1acf8e0aa1
aswinb-8 <aswin.b2022csbs@sece.ac.in>
2025-12-05 14:27:05 +0530

    Adding Third Program

---
+++ Program3.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int []arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(UserMainCode.findTriplet(arr));
+		}
+	}
+	
+class UserMainCode{
+	public static boolean findTriplet(int [] arr){
+		int l=arr.length;
+		for(int i=0;i<l-2;i++){
+			if((arr[i]==arr[i+1])&&(arr[i+1]==arr[i+2])){
+				return true;
+			}
+		}
+		return false;
+	}
+}
+	
